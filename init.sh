4c70e6365f8ea5e01bb0d0f714a0eca824c4ef2a
Sam4uk <sto.ukr.net@gmail.com>
2024-11-20 23:48:57 +0200

    Update init.sh
    Виправлен помилка, яка виникає при заміні файлу `vimrc`

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
-sh -c "$(wget https://raw.githubusercontent.com/Sam4uk/deputy/refs/heads/master/deputy -O -)" --  $(pwd) ~/.vim
-ln -s "$(pwd)/init.vim" "$(pwd)/vimrc"
-sh -c "$(wget https://raw.githubusercontent.com/Sam4uk/deputy/refs/heads/master/deputy -O -)" --  $(pwd) ~/.config/nvim
+sh -c "$(wget https://raw.githubusercontent.com/Sam4uk/deputy/refs/heads/master/deputy -O -)" -- "$(pwd) ~/.vim"
+sh -c "$(wget https://raw.githubusercontent.com/Sam4uk/deputy/refs/heads/master/deputy -O -)" -- "$(pwd)/init.vim" "$(pwd)/vimrc"
+sh -c "$(wget https://raw.githubusercontent.com/Sam4uk/deputy/refs/heads/master/deputy -O -)" -- "$(pwd)" "~/.config/nvim"
