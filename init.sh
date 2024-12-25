35066e1db96ea44a95727646e1070a5c331adf3a
Sam4uk <sam4uk.site@gmail.com>
2024-12-25 12:23:16 +0200

    середа, 25 грудня 2024 12:23:16 +0200

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
-sh -c "$(wget https://raw.githubusercontent.com/Sam4uk/deputy/refs/heads/master/deputy -O -)" -- "$(pwd) ~/.vim"
+sh -c "$(wget https://raw.githubusercontent.com/Sam4uk/deputy/refs/heads/master/deputy -O -)" -- "$(pwd)" "~/.vim"
 sh -c "$(wget https://raw.githubusercontent.com/Sam4uk/deputy/refs/heads/master/deputy -O -)" -- "$(pwd)/init.vim" "$(pwd)/vimrc"
 sh -c "$(wget https://raw.githubusercontent.com/Sam4uk/deputy/refs/heads/master/deputy -O -)" -- "$(pwd)" "~/.config/nvim"
